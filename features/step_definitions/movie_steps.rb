0348b485be30ca26889bcc16c928d627b58a283d
Angel Gil <angelgil87@gmail.com>
2013-04-20 19:19:07 -0700

    Homework number 3 completed.

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,7 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-    mov = Movie.new(:title => movie[:title], :rating => movie[:rating], :release_date => movie[:release_date])
-    mov.save
+    Movie.create(movie)
   end
   #flunk "Unimplemented"
 end
@@ -16,7 +15,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  assert page.body =~ %r{#{e1}.*#{e2}}m
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -27,4 +26,21 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(/\s*,\s*/).each do |rating|
+    if uncheck
+      step %Q{I uncheck "ratings_#{rating}"}
+    else
+      step %Q{I check "ratings_#{rating}"}
+    end
+  end
+end
+
+Then /(?:|I )should (not )?see all of the movies/ do |not_seen|
+  Movie.all.each do |movie|
+    if not_seen
+      step %Q{I should not see "#{movie.title}"}
+    else
+      step %Q{I should see "#{movie.title}"}
+    end
+  end
 end
